cf4e498c63be300ead6166dfac35d8c1c895d4b4
crosson.david@gmail.com <crosson.david@gmail.com>
2016-05-29 11:01:52 +0200

    first commit

---
+++ src/main/java/mtestdep/App.java
@@ -0,0 +1,13 @@
+package mtestdep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
